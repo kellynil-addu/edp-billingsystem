b7ed590d356fbde13fc7cc318c0dbc9bef79d9da
Kelly Neil Ang <angkellyn14@gmail.com>
2026-04-21 01:20:06 +0800

    Fix header shadow going behind content

--- src/components/ContentHeader.jsx
+++ src/components/ContentHeader.jsx
@@ -4,6 +4,7 @@ export default function ContentHeader({children}) {
         flex: "4rem 0 0", 
         backgroundColor: "var(--primary-background)",
         boxShadow: "0px 0px 5px rgb(0,0,0,0.2)",
+        zIndex: "5"
     };
 
     return (
